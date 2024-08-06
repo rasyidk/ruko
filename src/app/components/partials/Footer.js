b95ebffe587c1c4cc92cd1cbc597d03d1622a3fc
pudyasta <pudyastasatria@gmail.com>
2024-08-06 14:23:07 +0700

    fix[beranda]:layouting

--- src/app/components/partials/Footer.js
+++ src/app/components/partials/Footer.js
@@ -23,32 +23,27 @@ const medsos = [
     type: "instagram",
     link: "https://www.instagram.com/porsenigama/",
     icon1: "type1_instagram",
-    icon: <SiInstagram />,
   },
   {
     type: "tiktok",
     link: "https://www.tiktok.com/@porsenigama",
     icon1: "type1_tiktok",
-    icon: <SiTiktok />,
   },
   {
     type: "youtube",
     link: "https://www.youtube.com/@porsenigamatv3221",
     icon1: "type1_youtube",
-    icon: <SiYoutube />,
   },
 
   {
     type: "twitter",
     link: "https://twitter.com/porsenigama",
     icon1: "type1_x",
-    icon: <SiX />,
   },
   {
     type: "line",
     link: "https://page.line.me/porsenigama?openQrModal=true",
     icon1: "type1_line",
-    icon: <SiLine />,
   },
 ];
 
@@ -93,8 +88,16 @@ export function Footer({ type }) {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <div className="hover:bg-green-30 flex w-[24px] items-center justify-center ">
-                    {link.icon}
+                  <div className="hover:bg-green-30 flex w-[50px] items-center justify-center ">
+                    <Image
+                      src={
+                        "/assets/sosmed/" +
+                        link.type.toLocaleUpperCase() +
+                        ".png"
+                      }
+                      width={100}
+                      height={100}
+                    />
                   </div>
                 </a>
               ))}
--- src/app/components/partials/Navbar.js
+++ src/app/components/partials/Navbar.js
@@ -1,16 +1,22 @@
+"use client";
+
+import Aos from "aos";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Navbar() {
+  useEffect(() => {
+    Aos.init({});
+  });
   return (
-    <div className=" bg-white absolute z-20 w-full">
+    <div className=" bg-white  z-20 w-full fixed">
       <div className="container mx-auto flex justify-between items-center h-full">
         <div className="w-full flex justify-center lg:justify-start my-4">
           <Image
-            src="/assets/logo.svg" // Path to your logo image in the public directory
+            src="/assets/logo.png" // Path to your logo image in the public directory
             alt="Logo"
-            width={80} // Adjust the width of the image
-            height={50} // Adjust the height of the image
+            width={100} // Adjust the width of the image
+            height={100} // Adjust the height of the image
           />
         </div>
       </div>
