9a7535cb7ca94762e307736abdfc3541aa7618d1
gnafhan <nafhanghifari@gmail.com>
2024-08-25 21:50:43 +0700

    fix image bg

--- src/app/components/CobaGratis/CobaGratis.js
+++ src/app/components/CobaGratis/CobaGratis.js
@@ -7,7 +7,7 @@ export default function CobaGratis() {
                 className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
                 <div
                     className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
-                    <img src="assets/coba.jpg" alt=""
+                    <img src="assets/coba.png" alt=""
                          className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"/>
                 </div>
                 <div
