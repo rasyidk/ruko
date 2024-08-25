f50d8fe54400b3c4e098ee7f22f305e3253cc717
gnafhan <nafhanghifari@gmail.com>
2024-08-25 21:42:27 +0700

    fix scaling card

--- src/database/service.js
+++ src/database/service.js
@@ -16,7 +16,7 @@ export const services = [
     link: "https://wa.me/6281328404065?text=Halo%20Rumah%20Korea%20Indonesia%2C%20saya%20mau%20tanya%20tentang%20kursus%20Bahasa%20Korea%20Umum."
   },
   {
-    title: "Terjemahan (Korea-Indonesia/Indonesia-Korea)",
+    title: "Terjemahan (Korea-Indonesia / Indonesia-Korea)",
     slug: "terjemahan",
     content: `EPS TOPIK adalah suatu sistem ujian khusus bagi calon pekerja migran yang diadakan oleh HRD KOREA SELATAN sejak agustus 2005 untuk menyeleksi para calon pekerja asing secara adil dan terbuka serta mendorong pekerja asing untuk dapat beradaptasi lebih awal saat bekerja di Korea Selatan. 
     Data EPS TOPIK dalam website ini dibuat dengan bentuk soal terbarukan, sehingga akan sangat membantu anda dalam menghadapi soal-soal Ujian EPS-TOPIK yang sebenarnya`,
